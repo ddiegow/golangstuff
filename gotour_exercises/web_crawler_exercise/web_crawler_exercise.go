2baadfb95e898c7c29817c2e475ba8d90db76fa0
Daniel Diego Wildin <ddiegow@uoc.edu>
2023-04-24 00:05:54 +0900

    Create solution for web crawler. Need to fix bug

--- gotour_exercises/web_crawler_exercise/web_crawler_exercise.go
+++ gotour_exercises/web_crawler_exercise/web_crawler_exercise.go
@@ -1,7 +1,10 @@
 package main
 
+// TODO: rewrite to use worker-coordinator system using channels
+// TODO: figure out that found: https://golang.org/cmd/ "" output
 import (
 	"fmt"
+	"sync"
 )
 
 type Fetcher interface {
@@ -9,26 +12,58 @@ type Fetcher interface {
 	// a slice of URLs found on that page.
 	Fetch(url string) (body string, urls []string, err error)
 }
+type SafePool struct {
+	mu    sync.Mutex
+	pool  map[string]int
+	found map[string]int
+}
+
+func worker(safePool *SafePool, fetcher Fetcher) { // first thread
+	for { // infinite loop
+
+		safePool.mu.Lock()
+		if len(safePool.pool) == 0 {
+			safePool.mu.Unlock()
+			break
+		}
+
+		var theUrl string
+		for k := range safePool.pool {
+			theUrl = k // get the first key
+			break
+		}
+		theDepth := safePool.pool[theUrl]
+		delete(safePool.pool, theUrl)
+
+		if theDepth > 0 && len(theUrl) > 0 {
+			body, urls, err := fetcher.Fetch(theUrl)
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Printf("found: %s %q\n", theUrl, body)
+			safePool.found[theUrl] = 1
+			for _, u := range urls {
+				safePool.pool[u] = theDepth - 1
+			}
+		}
+		safePool.mu.Unlock()
+
+	}
+}
 
 // Crawl uses fetcher to recursively crawl
 // pages starting with url, to a maximum of depth.
 func Crawl(url string, depth int, fetcher Fetcher) {
+	fmt.Println("Starting crawler")
 	// TODO: Fetch URLs in parallel.
 	// TODO: Don't fetch the same URL twice.
 	// This implementation doesn't do either:
-	if depth <= 0 {
-		return
-	}
-	body, urls, err := fetcher.Fetch(url)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("found: %s %q\n", url, body)
-	for _, u := range urls {
-		Crawl(u, depth-1, fetcher)
-	}
-	return
+	// create a pool that will hold all the urls that need processing
+	safePool := SafePool{pool: make(map[string]int), found: make(map[string]int)}
+	safePool.pool[url] = depth
+	go worker(&safePool, fetcher)
+	go worker(&safePool, fetcher)
+	fmt.Scanln()
 }
 
 func main() {
@@ -83,3 +118,12 @@ var fetcher = fakeFetcher{
 		},
 	},
 }
+
+/*
+EXPECTED RESULTS
+found: https://golang.org/ "The Go Programming Language"
+found: https://golang.org/pkg/ "Packages"
+not found: https://golang.org/cmd/
+found: https://golang.org/pkg/fmt/ "Package fmt"
+found: https://golang.org/pkg/os/ "Package os"
+*/
